c3d4722a976a2c996607a4c021e0e58f9804524a
manoj <manoj@gmail.com>
2024-12-08 18:38:52 +0530

    Removed and added the files

--- src/main/java/Feature/Practise.feature
+++
@@ -1,6 +0,0 @@
-Feature: Check the feature once
-
-Scenario: Testing the functionality
-Given User is in the login page
-Then Open the browser
-And close the browser 
--- src/main/java/StepDefinition/StepPass_Fail.java
+++ src/main/java/StepDefinition/StepPass_Fail.java
@@ -2,16 +2,17 @@ package StepDefinition;
 
 
 
-import io.cucumber.java.After;
+
 import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 
 public class StepPass_Fail {
-	public static Scenario scenario=null;
+	public static  Scenario scenario;
 	@Before
 	public void beforeStep(Scenario scenario) {
 		this.scenario = scenario; 
+		
 		}
 	@AfterStep
 	public void tearDown(Scenario scenario) {
